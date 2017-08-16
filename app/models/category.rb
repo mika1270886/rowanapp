b8fc3056cde4dbf5a8d8570d5eefbc23f8261d53
Mika H <mika1270886@gmail.com>
2017-08-16 03:47:23 -0500

    commit to git

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+	attr_accessable :name
+	has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+	attr_accessable :name, :bod, :category_id, :author_id
+	belongs_to :category
 end
